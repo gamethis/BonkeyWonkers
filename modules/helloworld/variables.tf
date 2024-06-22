cada52d481da1225adada08b98fd13461f106031
John Whitley <johnewhitley@gmail.com>
2024-06-22 03:34:52 +0000

    adding stress container dockerfile and changing readme to ref it

--- modules/helloworld/variables.tf
+++ modules/helloworld/variables.tf
@@ -2,3 +2,8 @@ variable "source_file" {
   type        = string
   description = "Path of the source go file to run in Docker."
 }
+
+variable "commands" {
+  type = list(string)
+  default = ["whoami", "pwd", "ls -ltra"]
+}
--- modules/module2/main.tf
+++ modules/module2/main.tf
@@ -2,3 +2,8 @@ resource "local_file" "file3" {
   content  = "I'm the best file, I'm File 3"
   filename = "${path.cwd}/file3.txt"
 }
+
+resource "local_file" "file2" {
+  content  = "Well Hello There, I'm File 2"
+  filename = "${path.cwd}/file2.txt"
+}
