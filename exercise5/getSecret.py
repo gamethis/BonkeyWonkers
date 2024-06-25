bff898da30edc308b70e78193b5239fad21e8ccc
Larry <34108925+gamethis@users.noreply.github.com>
2024-06-25 02:26:05 +0000

    accept lint

--- exercise5/getSecret.py
+++ exercise5/getSecret.py
@@ -7,9 +7,9 @@ client = hvac.Client(
 )
 
 # Reading a secret
-read_response = client.secrets.kv.read_secret_version(path='')
+read_response = client.secrets.kv.read_secret_version(path="")
 
-data = read_response['data']['data']['']
+data = read_response["data"]["data"][""]
 
 # If this works properly, the secret you stored in vault prints
 print("Secret is: " + data)
