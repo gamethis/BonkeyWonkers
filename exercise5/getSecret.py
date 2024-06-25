dd3d36c7789ed1569d527de22c28a2c962454813
John Whitley <johnewhitley@gmail.com>
2024-06-24 22:24:18 -0400

    Fixex (#19)
    * Update README.md
    
    * Update getSecret.py
    
    * fixing trailing white space
    
    * Update getSecret.py

--- exercise5/getSecret.py
+++ exercise5/getSecret.py
@@ -8,8 +8,8 @@ client = hvac.Client(
 )
 
 # Reading a secret
-read_response = client.secrets.kv.read_secret_version(path='') # You'll need to specify the secret name you choose
+read_response = client.secrets.kv.read_secret_version(path='')
 
-data = read_response['data']['data'][''] # You'll need to specify the key you choose
+data = read_response['data']['data']['']
 
 print('Secret is: ' + data) # If this works properly, the secret you stored in vault prints
