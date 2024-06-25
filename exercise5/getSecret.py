f7b1185ac3bc90682ee6fd45c76189e351153a79
Larry <34108925+gamethis@users.noreply.github.com>
2024-06-24 21:36:43 -0500

    Updated linting (#17)
    * Linted and added wireshark
    
    * updated with comment
    
    * Update helloworld.py
    
    * accept lint

--- exercise5/getSecret.py
+++ exercise5/getSecret.py
@@ -1,15 +1,15 @@
 import hvac
-import sys
 
 # Authentication
 client = hvac.Client(
-    url='http://127.0.0.1:8200',
-    token='',
+    url="http://127.0.0.1:8200",
+    token="",
 )
 
 # Reading a secret
-read_response = client.secrets.kv.read_secret_version(path='')
+read_response = client.secrets.kv.read_secret_version(path="")
 
-data = read_response['data']['data']['']
+data = read_response["data"]["data"][""]
 
-print('Secret is: ' + data) # If this works properly, the secret you stored in vault prints
+# If this works properly, the secret you stored in vault prints
+print("Secret is: " + data)
