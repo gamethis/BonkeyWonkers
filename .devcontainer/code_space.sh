ea3d54a0f026eac1e633946766d2f4ea813a5e38
Ashley Eckard <a.eckard@hotmail.com>
2024-07-16 15:10:17 +0000

    ex2 and 3 readme updates

--- .devcontainer/code_space.sh
+++ .devcontainer/code_space.sh
@@ -78,13 +78,6 @@ echo "============"
 echo "Get test container"
 docker pull dahicks/sample:latest
 
-echo "Prep Excercise3"
-docker create --name excercise3 dahicks/sample:latest
-docker cp excercise3:/exercises/README.md /workspaces/BonkeyWonkers/exercise3/Instructions.md
-docker cp excercise3:/exercises/config.json.tmpl /workspaces/BonkeyWonkers/exercise3/config.json.tmpl
-docker rm -f dummy
-
-
 echo "Get stress test"
 docker pull j0hnewhitley/docker-stress:v0.0.1
 
