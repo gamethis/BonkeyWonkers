5bc69f7141028c7827c1eb3ac0a2b6dee6eaa94e
AXE5661_thdgit <ashley_eckard1@homedepot.com>
2024-07-16 10:11:50 -0400

    rm tf from ex2

--- exercise2/main.tf
+++
@@ -1,9 +0,0 @@
-module "service_config" {
-  source      = "../modules/helloworld"
-  source_file = "helloworld.py"
-}
-
-resource "local_file" "dockerfile" {
-  content  = module.service_config.service
-  filename = "${path.module}/Dockerfile"
-}
--- exercise2/providers.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "2.4.0"
-    }
-  }
-  required_version = ">=v1.0.5"
-}
